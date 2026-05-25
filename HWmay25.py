0445a8bca13241412f8f895ed98709ebd29bdece
harshada <harshamicrosoft26@gmail.com>
2026-05-25 21:40:54 +0530

    .

--- HWmay25.py
+++ HWmay25.py
@@ -18,18 +18,6 @@ for i in range(len(salary)):
 avg = total / len(salary)
 print(f"Avg salary of employees is {avg}")
 
-for i in range(len(salary)):
-    if salary[i] < avg:
-        if salary[i] == minimum:
-            bonus = minimum * 10/100 + maximum * 5/100
-            print(f"salary of minimum:{salary[i]} \t bonus:{bonus}")
-        bonus = salary[i] * 10/100
-        print(f"salary :{salary[i]} \t bonus:{bonus}")
-        
-    if salary[i] >=  avg:
-        if salary[i] == maximum:
-            print("no bonus for maximum salary!!")
-        bonus = salary[i] * 5/100
-        print(f"salary :{salary[i]} \t bonus:{bonus}")
+
        
 
